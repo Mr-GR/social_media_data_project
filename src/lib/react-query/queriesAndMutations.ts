ab678cd988f7fd51020054429bc0e453222f0c1c
Manuel Guevara Reyes <manny2144@icloud.com>
2024-12-04 15:04:52 -0500

    Add pages for serotonin

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useInfiniteQuery,
     useQueryClient
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
@@ -18,3 +18,9 @@ export const useSignInAccount = () => {
         mutationFn: (user: {email: string; password: string }) => signInAccount(user)
     })
 }
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount,
+    });
+};
