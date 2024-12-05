d284bce6d02b02860d82ed9c5532a0efa0c7e9b2
Manuel Guevara Reyes <manny2144@icloud.com>
2024-12-05 01:28:13 -0500

    Add navigation, database interactions for post, and post creation

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,21 +1,21 @@
-import Bottombar from "@/components/shared/Bottombar"
-import LeftSidebar from "@/components/shared/LeftSidebar"
-import TopBar from "@/components/shared/TopBar"
-import { Outlet } from "react-router-dom"
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import TopBar from "@/components/shared/TopBar";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className="w-full md:flex">
+    <div className="w-full flex flex-col min-h-screen">
       <TopBar />
-      <LeftSidebar />
-
-      <section className="flex flex-1 h-full">
-        <Outlet />
-      </section>
-
+      <div className="flex flex-1">
+        <LeftSidebar />
+        <section className="flex flex-1">
+          <Outlet />
+        </section>
+      </div>
       <Bottombar />
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -2,10 +2,11 @@ import {
     useQuery,
     useMutation,
     useInfiniteQuery,
-    useQueryClient
+    useQueryClient, 
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, signInAccount, signOutAccount, getPostById, getRecentPosts, updatePost } from '../appwrite/api'
+import { INewPost, INewUser, IUpdatePost } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -22,5 +23,45 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount,
+    });
+};
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        }
+    })
+}
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts,
+    })
+}
+
+export const useGetPostById = (postId?: string) => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+      queryFn: () => getPostById(postId),
+      enabled: !!postId,
+    });
+};
+
+export const useUpdatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: IUpdatePost) => updatePost(post),
+        onSuccess: (data) => {
+        queryClient.invalidateQueries({
+            queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+        });
+        },
     });
 };
